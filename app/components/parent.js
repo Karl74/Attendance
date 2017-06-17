9c035babeac8bbad1ca529751a42e497022daf87
Karl74 <carlos.castro74@gmail.com>
2017-06-17 12:49:21 -0500

    initial files

---
+++ app/components/parent.js
@@ -0,0 +1,20 @@
+var React = require("react");
+
+var Results = require("./Results");
+var Saved = require("./Saved");
+var Search = require("./Search");
+var helpers = require("./utilities/helpers.js");
+
+class Parent extends React.Component{
+
+render() {
+
+return (
+    //========== Hey Kathryn Look!!!!!!!   your code goes here :)
+
+
+);
+
+} //end of render function
+
+} // end of react class
